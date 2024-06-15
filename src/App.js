ed06eb4b6784d6328e1353feba31d0bdb1371d1f
KrishDevCrafting <krishkumer150@gmail.com>
2024-06-15 23:53:01 +0530

    asgh

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// src/index.js or src/index.tsx
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+    <>
+
+        <div className='text-danger'>
+
+            <h1>
+                DO it for You're Name..!
+            </h1>
+        </div>
+
+    </>)
+}
+
+export default App;
