9881f8490f46d461e9a2570ed354b5d86f00a5dd
KrishDevCrafting <krishkumer150@gmail.com>
2024-06-17 13:56:31 +0530

    updated-value

--- src/Components/Header/Nav.jsx
+++ src/Components/Header/Nav.jsx
@@ -1,25 +1,42 @@
-import React from 'react';
+import React from "react";
 // import CreateAccount from "../LoginSignup/CreateAccount";
-// import "./Nav.css"
-import img from "../Assets/Union-logo.png"
+import "./Nav.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import img from "../Assets/Union-logo.png";
 const NavbarComponent = () => {
-    return ( <>
-    
-    <div className='container d-flex justify-content-between m-4 border'>
+  return (
+    <>
+      <div className="container d-flex justify-content-between  border">
+        <div className="nav-box">
+          <a
+            className="h3"
+            href="http://"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ATG.<img src={img} alt="Img-Logo"></img>WRLD
+          </a>
+        </div>
+        <div className="nav-box">
+          <input
+            type="search"
+            name=""
+            id="search"
+            placeholder="Search for your favorite groups in ATG"
+          />
+          <FontAwesomeIcon id="icon-search" icon={faMagnifyingGlass} />
+        </div>
+        <div className="nav-box">
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <button>Create account. It’s free!</button>
+          </a>
+        </div>
+      </div>
 
-<div className="nav-box"><a href="http://" target="_blank" rel="noopener noreferrer">ATG.<img src={img} alt='Img-Logo'></img>WRLD</a></div>
-<div className="nav-box"><input type="search" name="" id=""placeholder='Search for your favorite groups in ATG' /></div>
-<div className="nav-box"><a href="http://" target="_blank" rel="noopener noreferrer"><button>Create account. It’s free!</button></a></div>
+      {/* <CreateAccount/> */}
+    </>
+  );
+};
 
-
-
-    </div>
-   
-    
-    
-    {/* <CreateAccount/> */}
-    
-    </> );
-}
- 
 export default NavbarComponent;
